78fbfd6735936b3fc316caeafc02bbb073c265c0
이성규 <seong9945@naver.com>
2018-11-27 20:53:45 +0900

    Create

---
+++ 이성규 - HH07.과제/이성규 - HH07.과제/Enemy.cpp
@@ -0,0 +1,38 @@
+#include "Enemy.h"
+#include "InputHandler.h"
+
+Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams)
+{
+
+}
+
+void Enemy::draw()
+{
+	SDLGameObject::draw(); // we now use SDLGameObject
+}
+
+void Enemy::update()
+{
+	m_position.setX(m_position.getX() + 1);
+	m_position.setY(m_position.getY() + 1);
+
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Enemy::clean()
+{
+
+}
+
+void Enemy::handleInput()
+{
+	if (TheInputHandler::Instance()->getMouseButtonState(LEFT))
+	{
+		m_velocity.setX(1);
+	}
+
+	// void Enemy::handleInput() 추가 : 마우스 motion
+	Vector2D* vec = TheInputHandler::Instance()->getMousePosition();
+	m_velocity = (*vec - m_position) / 100;
+
+}
